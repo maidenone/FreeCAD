41f8ca16afc704906f08f37bd5c8d3f58ccea13d
Zheng, Lei <realthunder.dev@gmail.com>
2017-04-29 18:27:43 +0800

    PropertyContainer: support dynamic property status
    Previously PropertyContainer/DocumentObject only checks property type
    for ReadOnly and Transient. Now it also checks property status bit
    Immutable and newly added Transient bit. The property status bit
    ReadOnly is not used for compatibility reason, as it is currently used
    to mark read-only access through property editor rather than Python.

--- src/App/DocumentObjectPyImp.cpp
+++ src/App/DocumentObjectPyImp.cpp
@@ -531,6 +531,13 @@ int DocumentObjectPy::setCustomAttributes(const char* attr, PyObject *obj)
     try {
         Property* prop = getDocumentObjectPtr()->getDynamicPropertyByName(attr);
         if (prop) {
+            if(prop->testStatus(Property::Immutable) ||
+            (getDocumentObjectPtr()->getPropertyType(prop) & Property::ReadOnly))
+            {
+                std::stringstream s;
+                s << "'DocumentObject' attribute '" << attr << "' is read-only"; 
+                throw Py::AttributeError(s.str());
+            }
             prop->setPyObject(obj);
             return 1;
         }
@@ -545,7 +552,9 @@ int DocumentObjectPy::setCustomAttributes(const char* attr, PyObject *obj)
         s << "Attribute (Name: " << attr << ") error: '" << exc.what() << "' ";
         throw Py::AttributeError(s.str());
     }
-    catch (...) {
+    catch (Py::AttributeError &) {
+        throw;
+    }catch (...) {
         std::stringstream s;
         s << "Unknown error in attribute " << attr;
         throw Py::AttributeError(s.str());
@@ -555,8 +564,9 @@ int DocumentObjectPy::setCustomAttributes(const char* attr, PyObject *obj)
     Property *prop = getDocumentObjectPtr()->getPropertyByName(attr);
     if (prop) {
         // Read-only attributes must not be set over its Python interface
-        short Type =  getDocumentObjectPtr()->getPropertyType(prop);
-        if (Type & Prop_ReadOnly) {
+        if(prop->testStatus(Property::Immutable) ||
+           (getDocumentObjectPtr()->getPropertyType(prop) & Property::ReadOnly))
+        {
             std::stringstream s;
             s << "'DocumentObject' attribute '" << attr << "' is read-only"; 
             throw Py::AttributeError(s.str());
