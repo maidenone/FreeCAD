09efb10c9de2df6429371ef868111161c60e39e7
Zheng, Lei <realthunder.dev@gmail.com>
2017-06-17 03:05:49 +0800

    FeaturePython: allow overriding view provider type
    You can override view provider type in two ways
    
    1) calling Document.addObject() with 'viewType' keyword set to the view
    provider type of your choice
    
    2) calling Document.addObject() with 'attach' keyword set to True, and
    'objProxy' keyword set to the Python binding object that defines
    getViewProviderName() and attach() methods. The attach() method will be
    called when the the Python binding object is assigned to document
    object's Proxy attribute. And getViewProviderName() will be called
    before creating the view provider of the new object.
    
    See addObject's doc string for more information

--- src/App/Document.cpp
+++ src/App/Document.cpp
@@ -1396,7 +1396,8 @@ void Document::writeObjects(const std::vector<App::DocumentObject*>& obj,
     for (it = obj.begin(); it != obj.end(); ++it) {
         writer.Stream() << writer.ind() << "<Object "
         << "type=\"" << (*it)->getTypeId().getName()     << "\" "
-        << "name=\"" << (*it)->getNameInDocument()       << "\" ";
+        << "name=\"" << (*it)->getNameInDocument()       << "\" "
+        << "ViewType=\"" << (*it)->getViewProviderNameStored() << "\" ";
 
         // See DocumentObjectPy::getState
         if ((*it)->testStatus(ObjectStatus::Touch))
@@ -1442,13 +1443,14 @@ Document::readObjects(Base::XMLReader& reader)
         reader.readElement("Object");
         std::string type = reader.getAttribute("type");
         std::string name = reader.getAttribute("name");
+        const char *viewType = reader.hasAttribute("ViewType")?reader.getAttribute("ViewType"):0;
 
         try {
             // Use name from XML as is and do NOT remove trailing digits because
             // otherwise we may cause a dependency to itself
             // Example: Object 'Cut001' references object 'Cut' and removing the
             // digits we make an object 'Cut' referencing itself.
-            App::DocumentObject* obj = addObject(type.c_str(), name.c_str(), /*isNew=*/ false);
+            App::DocumentObject* obj = addObject(type.c_str(), name.c_str(), /*isNew=*/ false, viewType);
             if (obj) {
                 objs.push_back(obj);
                 // use this name for the later access because an object with
@@ -2370,7 +2372,7 @@ void Document::recomputeFeature(DocumentObject* Feat)
         _recomputeFeature(Feat);
 }
 
-DocumentObject * Document::addObject(const char* sType, const char* pObjectName, bool isNew)
+DocumentObject * Document::addObject(const char* sType, const char* pObjectName, bool isNew, const char *viewType)
 {
     Base::BaseClass* base = static_cast<Base::BaseClass*>(Base::Type::createInstanceByName(sType,true));
 
@@ -2421,6 +2423,12 @@ DocumentObject * Document::addObject(const char* sType, const char* pObjectName,
 
     // mark the object as new (i.e. set status bit 2) and send the signal
     pcObject->setStatus(ObjectStatus::New, true);
+
+    if(!viewType)
+        viewType = pcObject->getViewProviderNameOverride();
+    if(viewType) 
+        pcObject->_pcViewProviderName = viewType;
+
     signalNewObject(*pcObject);
 
     // do no transactions if we do a rollback!
@@ -2505,6 +2513,10 @@ std::vector<DocumentObject *> Document::addObjects(const char* sType, const std:
 
         // mark the object as new (i.e. set status bit 2) and send the signal
         pcObject->setStatus(ObjectStatus::New, true);
+
+        const char *viewType = pcObject->getViewProviderNameOverride();
+        pcObject->_pcViewProviderName = viewType?viewType:"";
+
         signalNewObject(*pcObject);
 
         // do no transactions if we do a rollback!
@@ -2556,6 +2568,10 @@ void Document::addObject(DocumentObject* pcObject, const char* pObjectName)
 
     // mark the object as new (i.e. set status bit 2) and send the signal
     pcObject->setStatus(ObjectStatus::New, true);
+
+    const char *viewType = pcObject->getViewProviderNameOverride();
+    pcObject->_pcViewProviderName = viewType?viewType:"";
+
     signalNewObject(*pcObject);
 
     // do no transactions if we do a rollback!
@@ -2580,6 +2596,9 @@ void Document::_addObject(DocumentObject* pcObject, const char* pObjectName)
         if (d->activeUndoTransaction)
             d->activeUndoTransaction->addObjectDel(pcObject);
     }
+
+    const char *viewType = pcObject->getViewProviderNameOverride();
+    pcObject->_pcViewProviderName = viewType?viewType:"";
 
     // send the signal
     signalNewObject(*pcObject);
